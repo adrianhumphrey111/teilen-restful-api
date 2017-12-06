3c8aadc7470b097ae4740a131fed7b4fafb7bb22
Adrian Humphrey <adrianhumphrey374@yahoo.com>
2017-12-06 05:44:07 -0800

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Dec 4, 2017
-
-@author: adrianhumphrey
-'''
-stripe_api_key_secret = "sk_live_XzQ2FQafRK92EmpMXJ624BOU"
-stripe_api_key_publish = "pk_live_1SGsf48hM8JYS40mgge6CR8l"
-
-fire_base_api_key = "AIzaSyCHlGF__xoYPRvEDoR7zWLQXQQvsMkbaE4"
